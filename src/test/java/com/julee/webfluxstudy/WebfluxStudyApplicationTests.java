cd56ec0d21021d0e6b50bd9662083fe945c799e4
jinjulee <genesis0210@gmail.com>
2022-06-23 23:29:11 +0900

    프로젝트 시작

---
+++ src/test/java/com/julee/webfluxstudy/WebfluxStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.julee.webfluxstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
